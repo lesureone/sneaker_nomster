d0bb8b711d2868153234dc7f63acd43b9b3fe1f6
Larry LeSure <lesure1@gmail.com>
2014-12-30 17:00:32 +0000

    Added image upload functionality to the web app

--- Gemfile
+++ Gemfile
@@ -6,6 +6,9 @@ gem 'rails', '4.0.1'
 # Use postgresql as the database for Active Record
 gem 'pg'
 
+# Use CarrierWave
+gem 'carrierwave'
+
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.0'
 
@@ -64,3 +67,4 @@ gem 'geocoder'
 
 # Use figaro
 gem "figaro", ">= 1.0.0"
+
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141229023900) do
+ActiveRecord::Schema.define(version: 20141230060605) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20141229023900) do
     t.integer  "place_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "picture"
   end
 
   add_index "photos", ["place_id"], name: "index_photos_on_place_id", using: :btree
@@ -63,6 +64,7 @@ ActiveRecord::Schema.define(version: 20141229023900) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "picture"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
