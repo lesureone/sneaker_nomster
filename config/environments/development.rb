861512a38b6fae15b68ca8ddf5809256f46bc240
Larry LeSure <lesure1@gmail.com>
2014-12-31 01:25:10 +0000

    Setup automatic email using mailcatcher

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,8 @@
 SneakerNomster::Application.configure do
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+	config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
+	
+  # config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
@@ -15,7 +18,7 @@ SneakerNomster::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+	config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
